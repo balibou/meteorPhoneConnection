3c62002c6657e63f2261cb7974ebf442a1b2a90d
Benjamin Cherion <benjamin.cherion@gmail.com>
2016-12-13 17:28:52 +0000

    first commit

---
+++ client/main.js
@@ -0,0 +1,22 @@
+import { Template } from 'meteor/templating';
+import { ReactiveVar } from 'meteor/reactive-var';
+// import { Accounts } from 'accounts-phone';
+
+import './main.html';
+
+// Template.body.onCreated(function helloOnCreated() {
+//   var options = {phone:'+972545999999'};
+//   // You can also create user with password
+//   options.password = 'VeryHardPassword';
+//   Accounts.createUserWithPhone(options, function (){});
+//   // Debug: Verify the user phone isn't confirmed it.
+//   console.log('Phone verification status is :', Accounts.isPhoneVerified());
+// });
+//
+// Template.body.helpers({
+//   tasks: [
+//     { text: 'This is task 1' },
+//     { text: 'This is task 2' },
+//     { text: 'This is task 3' },
+//   ],
+// });
